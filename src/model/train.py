11091d1f50a8379bbda7b3640799a6a5f79952c1
nave cohen <nave.cohen@e.braude.ac.il>
2024-12-05 14:42:47 +0200

    feat: enhance training script with improved configuration handling and logging

--- src/model/train.py
+++ src/model/train.py
@@ -1,9 +1,9 @@
-
 import argparse
-from lightning_model import SegformerFinetuner
+from model.lightning_model import SegformerFinetuner
 from data.loader import Loader
-from utils import load_config, lc_id2label
+from utils import Config, lc_id2label
 from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pathlib import Path
 
@@ -14,11 +14,14 @@ def main():
         "--config", type=str, default="config.yaml", help="Path to config file")
     args = parser.parse_args()
 
-    config = load_config(args.config)
+    # Load configuration
+    config = Config(args.config)
 
-    # Create models dir
-    models_dir = Path(config["project"]["models_dir"])
+    # Create necessary directories
+    models_dir = Path(config.project.models_dir)
+    pretrained_dir = Path(config.project.pretrained_dir)
     models_dir.mkdir(parents=True, exist_ok=True)
+    pretrained_dir.mkdir(parents=True, exist_ok=True)
 
     # Prepare dataloaders
     loader = Loader(config)
@@ -28,25 +31,38 @@ def main():
     # Initialize model
     model = SegformerFinetuner(id2label=lc_id2label)
 
-    # Set up Trainer
-    checkpoint_callback = ModelCheckpoint(
-        dirpath=config["project"]["checkpoint_dir"], monitor="val_loss", save_top_k=1, mode="min"
+    # Define the logger
+    logger = TensorBoardLogger(
+        save_dir=Path(config.project.logs_dir),  # Log directory
+        default_hp_metric=False
     )
 
+    # Set up callbacks
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=f"{logger.save_dir}/{logger.version}/checkpoints",
+        monitor="val_loss",
+        save_top_k=1,
+        mode="min"
+    )
     early_stop_callback = EarlyStopping(
         monitor="val_loss",
-        patience=10,
-        mode="min",
+        patience=config.training.patience,
+        mode="min"
     )
 
+    # Initialize Trainer
     trainer = Trainer(
-        default_root_dir=config["project"]["logs_dir"],
-        max_epochs=config["training"]["max_epochs"],
+        logger=logger,
+        max_epochs=config.training.max_epochs,
         callbacks=[checkpoint_callback, early_stop_callback],
-        log_every_n_steps=10)
+        log_every_n_steps=10,
+    )
 
+    # Train the model
     trainer.fit(model, train_loader, val_loader)
-    model.save_pretrained_model(config["project"]["model_dir"])
+
+    # Save the trained model
+    model.save_pretrained_model(pretrained_dir, logger.version)
 
 
 if __name__ == "__main__":
