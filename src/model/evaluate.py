399d83fb323a794f32fa7b940faab8bc74455bbc
nave cohen <nave.cohen@e.braude.ac.il>
2024-12-05 14:42:24 +0200

    feat: enhance evaluation process with versioning and confusion matrix plotting

--- src/model/evaluate.py
+++ src/model/evaluate.py
@@ -1,7 +1,7 @@
 import argparse
 from model.lightning_model import SegformerFinetuner
 from src.data.loader import Loader
-from utils import load_config, lc_id2label
+from utils import Config, lc_id2label, find_checkpoint, plot_confusion_matrix
 from pytorch_lightning import Trainer
 from pathlib import Path
 
@@ -10,14 +10,17 @@ def main():
     parser = argparse.ArgumentParser(description="Evaluate SegFormer")
     parser.add_argument(
         "--config", type=str, default="default.yaml", help="Path to config file")
-    parser.add_argument("--checkpoint", type=str,
-                        required=True, help="Path to model checkpoint")
+    parser.add_argument("--version", type=str, default="version_0",
+                        help="Version of model to evaluate")
+
     args = parser.parse_args()
+    print(f"Evaluate {args.version} of model.")
+    # Load the configuration
+    config = Config(args.config)
 
-    # load config
-    config = load_config(args.config)
-
-    checkpoint = Path(config["project"]["checkpoint_dir"], args.checkpoint)
+    # Access paths and settings from the config
+    checkpoint = find_checkpoint(config, args.version)
+    print(f"Using checkpoint: {checkpoint}")
 
     # Prepare test dataloader
     loader = Loader(config)
@@ -25,11 +28,26 @@ def main():
 
     # Load model from checkpoint
     model = SegformerFinetuner.load_from_checkpoint(
-        checkpoint, lc_id2label)
+        checkpoint_path=checkpoint,
+        id2label=lc_id2label
+    )
 
     # Evaluate
     trainer = Trainer()
     trainer.test(model, test_loader)
+
+    # Collect predictions and ground truths
+    y_true = model.test_ground_truths
+    y_pred = model.test_predictions
+
+    # Create result dir
+    results_dir = Path(config.project.results_dir)
+    results_dir.mkdir(parents=True, exist_ok=True)
+
+    # Plot confusion matrix
+    labels = list(lc_id2label.values())  # Use class labels
+    cm_save_path = results_dir / f"{args.version}_confusion_matrix.png"
+    plot_confusion_matrix(y_true, y_pred, labels, save_path=cm_save_path)
 
 
 if __name__ == "__main__":
